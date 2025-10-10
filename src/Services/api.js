674989e4610a7ba8965975a9e2408e83e6ac50f6
naino <Hasnainkhan21>
2025-10-10 18:15:05 +0500

    frontend initailized

---
+++ src/Services/api.js
@@ -0,0 +1,16 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:3006/api",
+// });
+
+// // 🔐 Optional: Automatically attach JWT token (if logged in)
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
