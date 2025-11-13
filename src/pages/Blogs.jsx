97017d37c7daf21a42f4d234c0a81598433648e2
naino <Hasnainkhan21>
2025-11-13 23:00:40 +0500

    add loader

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,30 +1,50 @@
 import React, { useEffect, useState } from "react";
 import { getBlogs } from "../Services/blogService";
 import BlogCard from "../components/BlogCard";
+import { CircularProgress } from "@mui/material";
+import Navbar from "../components/Navbar";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchBlogs = async () => {
+      setLoading(true);
       try {
         const data = await getBlogs();
-        setBlogs(data);
+        const list = Array.isArray(data) ? data : data?.blogs || [];
+        setBlogs(list);
       } catch (err) {
-        console.error(err);
+        console.error("Failed fetching blogs:", err);
+        setBlogs([]);
+      } finally {
+        setLoading(false);
       }
     };
     fetchBlogs();
   }, []);
 
+  if (loading) {
+    return (
+
+      <div className="flex justify-center items-center h-64">
+        <CircularProgress />
+      </div>
+    );
+  }
+
   return (
+    <section>
+      <Navbar />
     <div className="container mx-auto px-4 py-8 grid md:grid-cols-2 lg:grid-cols-3 gap-6">
       {blogs.length > 0 ? (
-        blogs.map((blog) => <BlogCard key={blog._id} blog={blog} />)
+        blogs.map((blog) => <BlogCard key={blog._id || blog.id} blog={blog} />)
       ) : (
         <p className="text-center text-gray-500 col-span-full">No blogs found</p>
       )}
     </div>
+    </section>
   );
 };
 
