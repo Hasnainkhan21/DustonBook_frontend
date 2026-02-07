357adf3d9f42bcd408379471c8551f425452ad52
naino <hasnainmuhammad8425@gmail.com>
2026-02-07 21:50:58 +0500

    wattsapp button set

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -17,7 +17,7 @@ I’m interested in your books and would like more information.
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white px-5 py-3 rounded-full shadow-lg flex items-center gap-2 z-50"
     >
-      💬 WhatsApp
+    WhatsApp
     </a>
   );
 };
